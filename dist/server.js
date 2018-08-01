1d9c341cd8e923a3463a4e687716f6bc954e9e57
Merijn Hoogeveen <merijn@strangelove.nl>
2018-08-01 12:36:57 +0200

    Deploy build

--- dist/server.js
+++ dist/server.js
@@ -695,17 +695,10 @@ app.get('*', async function (req, res) {
   res.send(renderFullPage(html, initialData, helmetData, styles));
 });
 
-// const port = process.env.PORT || 9000;
-// app.listen(port, function () {
-// 	console.log('app running on localhost:' + port);
-// });
-
-var sslOptions = {
-  key: _fs2.default.readFileSync('./key.pem'),
-  cert: _fs2.default.readFileSync('./cert.pem')
-};
-
-_https2.default.createServer(sslOptions, app).listen(8443);
+var port = process.env.PORT || 9000;
+app.listen(port, function () {
+  console.log('app running on localhost:' + port);
+});
 
 function renderFullPage(html, preloadedState, helmet, styles) {
   return '\n    <!doctype html>\n    <html>\n      <head>\n        <link rel="icon" href="/dist/favicon.ico" type="image/ico" />\n        ' + helmet.title.toString() + '\n        ' + helmet.meta.toString() + '\n\t\t\t\t' + helmet.link.toString() + '\n\t\t\t\t' + styles + '\n      </head>\n      <body>\n        <div id="root">' + html + '</div>\n        <script>\n          // WARNING: See the following for security issues around embedding JSON in HTML:\n          // http://redux.js.org/docs/recipes/ServerRendering.html#security-considerations\n          window.__PRELOADED_STATE__ = ' + JSON.stringify(preloadedState).replace(/</g, '\\u003c') + '\n        </script>\n        <script src="/dist/assets/app.bundle.js"></script>\n      </body>\n    </html>\n    ';
